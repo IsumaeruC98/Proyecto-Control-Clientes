0f290620d410b3093cfe58176c3e58b4208d490c
IsumaeruC98 <44764254+IsumaeruC98@users.noreply.github.com>
2022-02-23 11:49:59 -0600

    actualizado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {
+    apiKey: 'AIzaSyCY89ELlNXOp9JV1ZjonjMQMYLE7T678bA',
+    authDomain: 'control-clientes-be8f0.firebaseapp.com',
+    projectId: 'control-clientes-be8f0',
+    storageBucket: 'control-clientes-be8f0.appspot.com',
+    messagingSenderId: '222807991197',
+    appId: '1:222807991197:web:57361cc671d5d5d6da8574',
+    measurementId: 'G-E9CCV99TB6',
+  }
 };
