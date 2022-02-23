f111134b288bd6a6a4b33d8fc7ccb2fc7fde1c65
IsumaeruC98 <44764254+IsumaeruC98@users.noreply.github.com>
2022-02-22 20:53:56 -0600

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,73 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { environment } from '../environments/environment';
+import { FlashMessagesModule } from 'flash-messages-angular';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+//Equivalentes
+// import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+// import { getAuth, provideAuth } from '@angular/fire/auth';
+// import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+// import { getStorage, provideStorage } from '@angular/fire/storage';  
+// import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+
+import { ClienteServicio } from './servicios/cliente.service';
+import { LoginService } from './servicios/login.service';
+import { AuthGuard } from './guardianes/auth.guard';
+import { ConfiguracionServicio } from './servicios/configuracion.service';
+import { ConfiguracionGuard } from './guardianes/configuracion.guard';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NoEncontradoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FlashMessagesModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firestore, "control-clientes"),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    //Equivalentes
+    // provideFirebaseApp(() => initializeApp(environment.firestore)),     
+    // provideFirestore(() => getFirestore()),  
+    // provideAuth(() => getAuth()),  
+    // provideStorage(() => getStorage()),  
+    // provideAnalytics(() => getAnalytics())
   ],
-  providers: [],
+  providers: [
+    ClienteServicio,
+    LoginService,
+    AuthGuard,
+    ConfiguracionServicio,
+    ConfiguracionGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
